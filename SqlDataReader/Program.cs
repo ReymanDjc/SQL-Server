6c7c81949c55cbc46aa09c7d1c1baad3e2a0e043
ReymanDJC <ReymanDJC@REYMANDJC-PC>
2017-01-21 17:48:45 -0400

    Add project files.

---
+++ SqlDataReader/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SqlDataReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
